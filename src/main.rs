dc071bf6398a207f2956c92c940f4d57b1e06e63
Dom Slee <domslee1@gmail.com>
2024-03-06 22:49:26 +1100

    clippy+fmt

--- src/main.rs
+++ src/main.rs
@@ -55,11 +55,11 @@ fn complete(root_args: &RootArgs, complete_args: &CompleteArgs) -> Result<(), st
 }
 
 fn maybe_process_path(arg: &str) -> String {
-    if arg.len() > 1 && arg.starts_with("`") && arg.ends_with("`") {
+    if arg.len() > 1 && arg.starts_with('`') && arg.ends_with('`') {
         // replace the start and end backticks with single quotes
         // also replace all single quotes with double single quote
-        let replaced_single_quotes = &arg[1..arg.len() - 1].replace("'", "''");
-        return format!("'{replaced_single_quotes}'", );
+        let replaced_single_quotes = &arg[1..arg.len() - 1].replace('\'', "''");
+        return format!("'{replaced_single_quotes}'",);
     }
 
     arg.to_string()
