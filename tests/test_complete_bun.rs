69d619894366852455e2e5c30243aa8e773c2c56
Dom Slee <domslee1@gmail.com>
2025-03-31 21:47:38 +1100

    Update

--- tests/test_complete_bun.rs
+++ tests/test_complete_bun.rs
@@ -1,9 +1,7 @@
 use rstest::rstest;
 use rstest_reuse::{self, *};
-use speculoos::assert_that;
 
 extern crate speculoos;
-use crate::speculoos::prelude::ContainingIntoIterAssertions;
 
 mod testenv;
 pub use testenv::*;
